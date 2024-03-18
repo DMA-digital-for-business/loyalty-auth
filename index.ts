74331ac2811638ddc1c46d2b837106f48b30232c
Matteo Collina <collinamatteo@gmail.com>
2024-03-18 18:06:59 +0100

    Fix ClientId, removed secret

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ dotenv.config();
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
 
@@ -15,12 +14,10 @@ const client = new CognitoIdentityServiceProvider();
 const login = async (
   username: string,
   password: string,
-  UserPoolId: string,
-  ClientId: string
 ): Promise<CognitoIdentityServiceProvider.InitiateAuthResponse> => {
   const params = {
     AuthFlow: 'USER_PASSWORD_AUTH',
-    ClientId,
+    ClientId: process.env.AWS_ACCESS_KEY_ID!,
     AuthParameters: {
       USERNAME: username,
       PASSWORD: password,
@@ -29,35 +26,13 @@ const login = async (
 
   try {
     const session = await client.initiateAuth(params).promise();
-    console.log(session);
+    // console.log(session);
     return session;
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     throw error;
   }
 };
 
-const freshToken = async (
-  refreshToken: string,
-  UserPoolId: string,
-  ClientId: string
-): Promise<CognitoIdentityServiceProvider.InitiateAuthResponse> => {
-  const params = {
-    AuthFlow: 'REFRESH_TOKEN_AUTH',
-    ClientId,
-    AuthParameters: {
-      REFRESH_TOKEN: refreshToken,
-    },
-  };
 
-  try {
-    const refreshedSession = await client.initiateAuth(params).promise();
-    console.log(refreshedSession);
-    return refreshedSession;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-export { login, freshToken };
+export { login };
