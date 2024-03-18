542e5ac0e4a6c7f7763a222be11fdda0b55dc19c
Matteo Collina <collinamatteo@gmail.com>
2024-03-18 23:57:15 +0100

    class

--- index.ts
+++ index.ts
@@ -1,38 +1,40 @@
 import * as AWS from 'aws-sdk';
 import { CognitoIdentityServiceProvider } from 'aws-sdk';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  region: process.env.AWS_REGION,
-});
-
-const client = new CognitoIdentityServiceProvider();
-
-const login = async (
-  username: string,
-  password: string,
-): Promise<CognitoIdentityServiceProvider.InitiateAuthResponse> => {
-  const params = {
-    AuthFlow: 'USER_PASSWORD_AUTH',
-    ClientId: process.env.AWS_ACCESS_KEY_ID!,
-    AuthParameters: {
-      USERNAME: username,
-      PASSWORD: password,
-    },
-  };
-
-  try {
-    const session = await client.initiateAuth(params).promise();
-    // console.log(session);
-    return session;
-  } catch (error) {
-    // console.error(error);
-    throw error;
+class LoyaltyAuth {
+  private _client:AWS.CognitoIdentityServiceProvider|null = null;
+  
+  public static factory(accessKeyId:string, region:string):LoyaltyAuth{
+    let _ = new LoyaltyAuth()
+    _._client = new AWS.CognitoIdentityServiceProvider({
+      accessKeyId: accessKeyId,
+      region: region
+    });
+    return _;
   }
-};
+
+  public async login(
+    username: string,
+    password: string,
+  ):Promise<CognitoIdentityServiceProvider.InitiateAuthResponse>{
+    const params = {
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: process.env.AWS_ACCESS_KEY_ID!,
+      AuthParameters: {
+        USERNAME: username,
+        PASSWORD: password,
+      },
+    };
+    try {
+      const session = await this._client!.initiateAuth(params).promise();
+      // console.log(session);
+      return session;
+    } catch (error) {
+      // console.error(error);
+      throw error;
+    }
+  }
+}
 
 
-export { login };
+export default LoyaltyAuth;
